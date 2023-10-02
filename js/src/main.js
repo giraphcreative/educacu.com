1131d9a0c405c1916ec45ef01e77497bdee19a5e
James Pederson <james@jpederson.com>
2023-10-02 13:17:31 +0000

    Add speedbump exception for new online branch.

--- js/src/main.js
+++ js/src/main.js
@@ -62,6 +62,7 @@ jQuery(document).ready(function($){
 			the_link_href.match( /shareteccu.com/i ) || 
 			the_link_href.match( /billpaysite.com/i ) || 
 			the_link_href.match( /gbs.onlinecu.com/i ) || 
+			the_link_href.match( /itsme247.com/i ) || 
 			the_link_href.charAt(0) === '#' || 
 			the_link_href.charAt(0) === '/' ) {
 			location.href = the_link_href;
