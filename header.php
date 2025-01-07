7d3e89a3d6a1a172a13b6ee6e67fc626d2f099d6
James Pederson <james@jpederson.com>
2025-01-07 16:33:57 +0000

    Swap back to normal logo.

--- header.php
+++ header.php
@@ -52,7 +52,7 @@ gtag('config', 'UA-97213155-1');
 	
 		<div class="logo">
 			<a href="/" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<h1><img src="<?php bloginfo( "template_url" ) ?>/img/logo-anniversary.png" alt="<?php bloginfo( 'name' ); ?>"></h1>
+				<h1><img src="<?php bloginfo( "template_url" ) ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>"></h1>
 			</a>
 		</div>
 		
