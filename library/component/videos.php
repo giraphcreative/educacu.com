598fe9ed0c1bbfb8ace9c375de86337291f949d4
James Pederson <james@jpederson.com>
2025-02-14 15:31:15 +0000

    Add titles for videos.

--- library/component/videos.php
+++ library/component/videos.php
@@ -7,9 +7,11 @@ if ( have_rows('video') ):
     // loop through the rows of data
     $num = 1;
     while ( have_rows( 'video' ) ) : the_row();
+        $title = get_sub_field( 'title' );
         ?>
     <div class="video">
         <?php the_sub_field('video'); ?>
+        <?php if ( !empty( $title ) ) { print '<p class="video-title">' . $title . '</p>'; } ?>
     </div>
         <?php
     endwhile;
